c3877c1bc85f45388ce6b1d30a3ef7a3ce081d95
Miguel da Mota <miguel@damota.de>
2025-04-07 09:53:58 +0200

    issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Layout",
+}
+
 export default function RootLayout({
   children,
+  routes,
 }: Readonly<{
   children: React.ReactNode;
+  routes: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* {children} */}
+        {routes}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My App",
+}
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <p>Hello from Home</p>
+  );
 }
